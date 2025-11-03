ead01ded7e369a6cec8c6c0dbf72b97672ee31dc
Andrey <artemov2mail@gmail.com>
2025-11-03 11:27:13 +0300

    Refactor: improved user prompts and fixed costume counting logic

--- halloween_costumes.cpp
+++ halloween_costumes.cpp
@@ -1,43 +1,61 @@
- #include <iostream>
+// File name: halloween_costumes.cpp
+
+#include <iostream>
 #include <vector>
-#include <stack>
 
 int minimal_costumes(int N, int M, const std::vector<int>& parties) {
-    std::stack<int> stk;
-    int costumes_count = 0;
+    std::vector<int> costumes_prep(M + 1, 0); // Amount of purchased costumes for each type
+    std::vector<int> stack; // Current stack of worn costumes
 
-    for (int costume_type : parties) {
-        // Remove costumes if the top one does not fit the party
-        while (!stk.empty() && stk.top() != costume_type) {
-            stk.pop();
-        }
-        // If there is no suitable costume on top, put on a new one
-        if (stk.empty() || stk.top() != costume_type) {
-            stk.push(costume_type);
-            costumes_count++;
+    for (int i = 0; i < N; ++i) {
+        int type = parties[i];
+
+        // Check if the required costume type is already on top
+        if (!stack.empty() && stack.back() == type) {
+            // Costume of the required type is already on top, do nothing
+        } else {
+            // Look for the required type deeper in the stack, remove costumes above it if present
+            int pos = -1;
+            for (int j = (int)stack.size() - 1; j >= 0; --j) {
+                if (stack[j] == type) {
+                    pos = j;
+                    break;
+                }
+            }
+            if (pos != -1) {
+                // Remove all costumes above the found position
+                stack.erase(stack.begin() + pos + 1, stack.end());
+            } else {
+                // Costume of required type is not present, purchase a new one
+                costumes_prep[type]++;
+            }
+            // Put the required type on top of the stack
+            stack.push_back(type);
         }
     }
-
-    return costumes_count;
+    int total = 0;
+    for (int i = 1; i <= M; ++i) total += costumes_prep[i];
+    return total;
 }
 
 int main() {
     int N, M;
-    std::cout << "Welcome to the Halloween Party Costume Planner!\n";
-    std::cout << "Please enter two integers (N and M):\n";
-    std::cout << "- N (number of parties, 2 <= N < 100)\n";
-    std::cout << "- M (number of costume types, 2 <= M < 100)\n";
-    std::cout << "Example: 5 3\n";
+    std::cout << "Welcome to the Halloween Costume Calculator!\n";
+    std::cout << "Please enter two integers separated by a space:\n";
+    std::cout << "N - number of parties (2 <= N < 100)\n";
+    std::cout << "M - number of costume types (2 <= M < 100)\n";
+    std::cout << "Example: 7 3\n";
     std::cin >> N >> M;
 
-    if (N < 2 || N >= 100 || M < 2 || M >= 100) {
-        std::cout << "Error: N and M must satisfy 2 <= N < 100, 2 <= M < 100.\n";
+    if (N < 1 || N > 100 || M < 1 || M > 100) {
+        std::cout << "Error: N and M must satisfy 1 <= N <= 100, 1 <= M <= 100.\n";
         return 1;
     }
 
     std::vector<int> parties(N);
-    std::cout << "Now enter " << N << " integers for party costume types (from 1 to " << M << "):\n";
-    std::cout << "Example: 1 2 3 2 1\n";
+    std::cout << "Now enter the sequence of costumes for each party, separated by spaces:\n";
+    std::cout << "(each value should be an integer between 1 and " << M << ")\n";
+    std::cout << "Example: 1 2 1 1 3 2 1\n";
     for (int i = 0; i < N; i++) {
         std::cin >> parties[i];
         if (parties[i] < 1 || parties[i] > M) {
@@ -52,4 +70,3 @@ int main() {
 
     return 0;
 }
-
